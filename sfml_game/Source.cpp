a3e11936c3343bb72e2d8eff8c8b2006680117cb
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2020-08-29 19:45:38 +0700

    -

--- sfml_game/Source.cpp
+++ sfml_game/Source.cpp
@@ -1,20 +1,76 @@
+
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Avoid Bullet");
+
+	////// Circle
+	sf::CircleShape collision(100.f);
+	collision.setPosition({ 200.f, 200.f });
+	collision.setFillColor(sf::Color::Red);
+
+	////// Texture
+	sf::Texture playerTexture;
+	if (!playerTexture.loadFromFile("C:\Users\Mr.Supakorn Thongbor\Downloads\1.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+
+	////// Sprites
+	sf::Sprite shapeSprite;
+	shapeSprite.setTexture(playerTexture);
+
+	int spriteSizeX = playerTexture.getSize().x / 3;
+	int spriteSizeY = playerTexture.getSize().y / 4;
+
+	shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
+
+	sf::Vector2f spawnPoint = { 0.f, 0.f };
+	shapeSprite.setPosition(spawnPoint);
+
+	int animationFrame = 0;
+
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		window.draw(shapeSprite);
+		window.draw(collision);
+		window.display();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			shapeSprite.move(.1f, 0.f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			shapeSprite.move(-.1f, 0.f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 3, spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			shapeSprite.move(0.f, -.1f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			shapeSprite.move(0.f, .1f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 2, spriteSizeX, 32));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+		animationFrame++;
+
+		if (animationFrame >= 2) {
+			animationFrame = 0;
+		}
+
+		if (collision.getGlobalBounds().intersects(shapeSprite.getGlobalBounds())) {
+			shapeSprite.setPosition(spawnPoint);
 		}
 		window.clear();
-		window.draw(shape);
-		window.display();
 	}
 	return 0;
 }
