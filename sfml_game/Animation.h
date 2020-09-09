e61ca278ec1449a5a2131cd5514823d2c33f933a
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2020-09-09 11:51:23 +0700

    -

--- sfml_game/Animation.h
+++ sfml_game/Animation.h
@@ -1,11 +1,18 @@
 #pragma once
+#include<SFML\Graphics.hpp>
 class Animation
 {
-	public Animation();
+	public Animation(sf::Texture* texture ,sf::Vector2u imageCout ,float switchTime);
 ~Animation();
 
+public:
+	sf::IntRect uvRect;
 
 private:
+	sf::Vector2u imageCout;
+	sf::Vector2u currentimage;
 
+	float totalTime;
+	float switchTime;
 };
 
