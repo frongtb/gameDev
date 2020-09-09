40070f058c06098ba7b4a1b9d813bea78a74c424
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2020-09-09 23:41:56 +0700

    -

--- sfml_game/Animation.cpp
+++ sfml_game/Animation.cpp
@@ -1,1 +1,20 @@
 #include "Animation.h"
+
+
+
+Animation::Animation(sf::Texture* texture, sf::Vector2u imageCout, float switchTime);
+{
+	this->imageCount = imageCount;
+	this->switchTime = switchTime;
+	totalTime = 0.00f;
+	currentimage.x = 0;
+
+
+}
+
+
+
+Animation::~Animation();
+{
+
+}
